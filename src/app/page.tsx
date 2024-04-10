85c8c02b9fa5ec8da5af697b06eb46a323a57898
ByronMike <auger.michaell@gmail.com>
2024-04-10 22:20:39 +0200

    feat: dynamic timeline section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { About } from '@/components/sections/about';
 import { Introduction } from '@/components/sections/introduction';
+import { Timeline } from '@/components/sections/timeline';
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-center">
       <Introduction />
       <About />
+      <Timeline />
     </main>
   );
 }
