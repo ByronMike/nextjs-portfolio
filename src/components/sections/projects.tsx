43fd84040706677ec71bd00eb88c0e5f67fe7dec
ByronMike <auger.michaell@gmail.com>
2024-04-14 15:01:20 +0200

    style: add glassmorphism on cards

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { FunctionComponent } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -5,6 +7,11 @@ import { LinkIcon } from '@heroicons/react/24/solid';
 import { FaGithub } from 'react-icons/fa';
 import clsx from 'clsx';
 import { PROJECTS } from '@/content/projects';
+import { motion } from 'framer-motion';
+
+const variants = {
+  hover: { opacity: 0, y: '100%' },
+};
 
 export const Projects: FunctionComponent = () => {
   return (
@@ -15,7 +22,7 @@ export const Projects: FunctionComponent = () => {
       <header className="mx-auto grid w-full max-w-6xl px-4 md:px-8">
         <div className="heading-pre">Mes projets</div>
         <h1 className="heading-2xl -ml-1">Une sélection de mes projets</h1>
-        <div className="mb relative -mx-4 mb-2 flex overflow-x-auto px-4 pb-2">
+        <div className="mb relative -mx-4 mb-2 flex gap-2 overflow-x-auto px-4 pb-2">
           {['All Projects', ...new Set(PROJECTS.map((p) => p.type).flat())].map(
             (type, index) => {
               return (
@@ -39,8 +46,9 @@ export const Projects: FunctionComponent = () => {
       <div className="flex flex-wrap gap-4">
         {PROJECTS.map((project) => {
           return (
-            <section
+            <motion.section
               key={project.name}
+              whileHover="hover"
               className={clsx(
                 'backdrop relative h-[380px] w-[340px] min-w-[340px] snap-start rounded-xl border-2 border-gray-700/30 bg-white bg-opacity-10  bg-clip-padding p-4 shadow-lg shadow-xl transition-[min-width,width,margin-left,opacity] duration-300 spacing-0 d:border-white/20',
                 project
@@ -57,12 +65,16 @@ export const Projects: FunctionComponent = () => {
                   className="rounded-t-lg object-cover object-center"
                 />
               </figure>
-              <div className="absolute bottom-0 left-0">
-                <header>
-                  <h2 className="text-2xl font-bold tracking-tighter text-gray-800 d:text-white">
+              <motion.div
+                variants={variants}
+                transition={{ duration: 0.3 }}
+                className="absolute bottom-0 left-0 w-full rounded-bl-xl rounded-br-xl bg-opacity-60 bg-clip-padding py-2 backdrop-blur-lg backdrop-filter"
+              >
+                <header className="px-4">
+                  <h2 className="text-2xl font-bold tracking-tighter text-black d:text-white">
                     {project.name}
                   </h2>
-                  <div className="-ml-0.5 mt-0.5 flex items-center gap-2 tracking-tight text-gray-600 d:text-gray-200">
+                  <div className="-ml-0.5 mt-0.5 flex items-center gap-2 tracking-tight text-black d:text-gray-200">
                     {project.tech?.map(({ name }, i) => {
                       if (i > 3) return null;
                       return (
@@ -76,11 +88,11 @@ export const Projects: FunctionComponent = () => {
                     })}
                   </div>
                 </header>
-                <main className="mt-2 text-[15px] tracking-tight text-gray-600 d:text-gray-200 ">
+                <main className="mt-2 px-4 text-[15px] tracking-tight text-black d:text-black ">
                   <p className="line-clamp-4">{project.description}</p>
                 </main>
-                <footer className="bottom-3 left-0 mt-auto flex w-full items-end justify-end gap-2 px-4">
-                  <div className="mr-auto text-sm font-semibold text-gray-700/80 d:text-gray-300/80">
+                <footer className="bottom-3 left-0 mt-auto flex w-full items-end justify-end gap-2 px-4 px-4">
+                  <div className="mr-auto text-sm font-semibold text-black d:text-gray-300/80">
                     {project.year}
                   </div>
                   {project.repository ? (
@@ -106,8 +118,8 @@ export const Projects: FunctionComponent = () => {
                     </Link>
                   ) : null}
                 </footer>
-              </div>
-            </section>
+              </motion.div>
+            </motion.section>
           );
         })}{' '}
       </div>
