8c8ca83c719e63691b47a8475bf38e0553466df9
ByronMike <auger.michaell@gmail.com>
2024-04-19 17:53:10 +0200

    feat: contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { TechStack } from '@/components/sections/tech-stack';
 import { Timeline } from '@/components/sections/timeline';
 import { Projects } from '@/components/sections/projects';
 import { Partners } from '@/components/sections/partners';
+import { Contact } from '@/components/sections/contact';
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <Timeline />
       <Projects />
       <Partners />
+      <Contact />
     </main>
   );
 }
---
+++ src/components/sections/contact.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { FunctionComponent } from 'react';
+
+import { CONTACTS } from '@/content/contact';
+
+export const Contact: FunctionComponent = () => {
+  return (
+    <section>
+      <div className="mx-auto mb-40 grid w-full max-w-6xl px-4 md:px-8">
+        <div className="heading-pre flex justify-center">Contact</div>
+        <h1 className="heading-2xl -ml-1 flex justify-center">Keep in Touch</h1>
+        <p className="tracking tight mb-2 text-center leading-relaxed text-gray-500 d:text-gray-100/70 [&>p+p]:mt-4">
+          N'hésitez pas à me contacter pour échanger sur mes projets ou pour
+          toutes autres opportunités professionnelles :
+        </p>
+        <div className="-ml-0.5 mt-0.5 flex items-center justify-center gap-3  tracking-tight text-black d:text-gray-200">
+          {CONTACTS.map(({ title, Icon, url }) => (
+            <a key={title} href={url}>
+              <div className="flex select-none items-center gap-1 whitespace-nowrap rounded border border-gray-700/10  bg-sky-500/90 px-1.5 py-[2px] text-[13px]  font-medium  text-white transition-colors hfa:bg-sky-600/80 d:bg-sky-600/90 d:hfa:bg-sky-500/80">
+                <Icon className="color-red" />
+                {title}
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
