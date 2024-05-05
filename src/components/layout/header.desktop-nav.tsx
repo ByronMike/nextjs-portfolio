53cfb35aa6740050ee8824cb63c40e561ac368f5
ByronMike <auger.michaell@gmail.com>
2024-05-05 15:00:38 +0200

    fix: header dark theme

--- src/components/layout/header.desktop-nav.tsx
+++ src/components/layout/header.desktop-nav.tsx
@@ -18,7 +18,7 @@ export const DesktopNav: FunctionComponent = () => {
               >
                 <Link
                   href={link.href}
-                  className={`relative flex cursor-pointer bg-clip-padding px-4 py-1.5 text-gray-500 after:absolute after:bottom-0 after:left-0 after:h-1 after:w-0 after:bg-blue-400 after:transition-all after:duration-500 hover:text-gray-900 hover:after:w-full`}
+                  className={`relative flex cursor-pointer bg-clip-padding px-4 py-1.5 text-gray-500 after:absolute after:bottom-0 after:left-0 after:h-1 after:w-0 after:bg-blue-400 after:transition-all after:duration-500 hover:text-gray-900 hover:after:w-full d:text-gray-300/90 d:hfa:text-gray-300`}
                 >
                   <span className="text-sm font-medium ">{link.title}</span>
                 </Link>
